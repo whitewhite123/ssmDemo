97a2eab1a8d0db05fbd36f8c36fc34f266b3c747
msj <1751871164@qq.com>
2019-07-03 14:19:40 +0800

    springmvc拦截器解决乱码

--- testDemo02/src/main/java/com/msj/TestInterceptor.java
+++ testDemo02/src/main/java/com/msj/TestInterceptor.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class TestInterceptor implements HandlerInterceptor {
     public boolean preHandle(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, Object handler) throws Exception {
+        request.setCharacterEncoding("utf-8");
         System.out.println("之前1");
         //返回值：表示我们是否要拦截当前的请求
         //true：请求被执行
